8f679a14c40772b62239c0e84033c164af6dc39e
amulya <amulyakasindula97@gmail.com>
2021-03-06 14:32:51 +0530

    first commit....

---
+++ src/main/java/Company/gitbashPractice/App.java
@@ -0,0 +1,13 @@
+package Company.gitbashPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
